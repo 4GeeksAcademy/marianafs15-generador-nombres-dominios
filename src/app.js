1dd3f83fe087d525371809c60310edb2d3faed05
Mariana Hueck <marianahueck@gmail.com>
2024-07-17 23:34:20 +0000

    borre prettier

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombre = ["the", "our"];
+  let adj = ["great", "big"];
+  let nombre = ["jogger", "racoon"];
+  let lista = document.querySelector(".lista");
+  let listaNombresDominios = [];
+  console.log(pronombre[0]);
+  console.log(pronombre[1]);
+  for (let i = 0; i < pronombre.length; i++) {
+    console.log(pronombre[i]);
+  }
 };
